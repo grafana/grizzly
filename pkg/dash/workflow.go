32ef9e1da5a414e21272468f5c6fbebb40b49ff0
Malcolm Holmes <mdh@odoko.co.uk>
2020-05-27 15:20:06 +0100

    Colourise

--- pkg/dash/workflow.go
+++ pkg/dash/workflow.go
@@ -6,8 +6,15 @@ import (
 	"log"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/kylelemons/godebug/diff"
 	"gopkg.in/fsnotify.v1"
+)
+
+var (
+	red    = color.New(color.FgRed).SprintFunc()
+	yellow = color.New(color.FgYellow).SprintFunc()
+	green  = color.New(color.FgGreen).SprintFunc()
 )
 
 // Get retrieves JSON for a dashboard from Grafana, using the dashboard's UID
@@ -39,7 +46,11 @@ func Show(config Config, jsonnetFile string, targets *[]string) error {
 	}
 
 	for name, board := range boards {
+<<<<<<< HEAD
 		fmt.Printf("== %s ==\n", name)
+=======
+		fmt.Println("Found", name)
+>>>>>>> Colourise
 		j, err := board.GetDashboardJSON()
 		if err != nil {
 			return err
@@ -82,9 +93,9 @@ func Diff(config Config, jsonnetFile string, targets *[]string) error {
 		existingBoardJSON, _ := existingBoard.GetDashboardJSON()
 
 		if boardJSON == existingBoardJSON {
-			fmt.Println(name, "no differences")
+			fmt.Println(name, yellow("no differences"))
 		} else {
-			fmt.Println(name, "changes detected:")
+			fmt.Println(name, red("changes detected:"))
 			difference := diff.Diff(existingBoardJSON, boardJSON)
 			fmt.Println(difference)
 		}
@@ -122,9 +133,9 @@ func Apply(config Config, jsonnetFile string, targets *[]string) error {
 		existingBoardJSON, _ := existingBoard.GetDashboardJSON()
 
 		if boardJSON == existingBoardJSON {
-			fmt.Println(name, "unchanged")
+			fmt.Println(name, yellow("unchanged"))
 		} else {
-			fmt.Println(name, "updated")
+			fmt.Println(name, green("updated"))
 
 			err = postDashboard(config, board)
 			if err != nil {
