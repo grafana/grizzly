0e4ae5dfe49390bb264658f1cd06bca7bd9ef31d
Malcolm Holmes <mdh@odoko.co.uk>
2020-10-27 14:24:30 +0000

    Flatten getWSGrafanaURL func

--- pkg/grafana/config.go
+++ pkg/grafana/config.go
@@ -27,30 +27,33 @@ func getGrafanaURL(urlPath string) (string, error) {
 }
 
 func getWSGrafanaURL(urlPath string) (string, string, error) {
-	if grafanaURL, exists := os.LookupEnv("GRAFANA_URL"); exists {
-		u, err := url.Parse(grafanaURL)
-		if err != nil {
-			return "", "", err
-		}
-		if u.Scheme == "https" {
-			u.Scheme = "wss"
-		} else {
-			u.Scheme = "ws"
-		}
-		u.Path = path.Join(u.Path, urlPath)
-		grafanaURL = u.String()
-		if token, exists := os.LookupEnv("GRAFANA_TOKEN"); exists {
-			u.User = nil
-			return u.String(), token, nil
-		} else if u.User != nil {
-			token, ok := u.User.Password()
-			if ok {
-				u.User = nil
-				return u.String(), token, nil
-			}
+	grafanaURL, exists := os.LookupEnv("GRAFANA_URL")
+	if !exists {
+		return "", "", fmt.Errorf("Require GRAFANA_URL (optionally GRAFANA_TOKEN if auth required) for websocket actions")
+	}
+	u, err := url.Parse(grafanaURL)
+	if err != nil {
+		return "", "", err
+	}
+	if u.Scheme == "https" {
+		u.Scheme = "wss"
+	} else {
+		u.Scheme = "ws"
+	}
+	u.Path = path.Join(u.Path, urlPath)
+	grafanaURL = u.String()
+	token, ok := os.LookupEnv("GRAFANA_TOKEN")
+	if ok {
+		u.User = nil
+		return u.String(), token, nil
+	}
+	if u.User != nil {
+		token, ok := u.User.Password()
+		if !ok {
 			return u.String(), "", nil
 		}
-		return u.String(), "", nil
+		u.User = nil
+		return u.String(), token, nil
 	}
-	return "", "", fmt.Errorf("Require GRAFANA_URL (optionally GRAFANA_TOKEN if auth required) for websocket actions")
+	return u.String(), "", nil
 }
