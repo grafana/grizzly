1754494189be2b865f0e50a90d327edeee3ae01e
Malcolm Holmes <mdh@odoko.co.uk>
2021-04-03 14:02:43 +0100

    Actually remove the Config struct

--- pkg/grizzly/config.go
+++ pkg/grizzly/config.go
@@ -1,11 +1,5 @@
 package grizzly
 
-// Config provides configuration to `grizzly`
-type Config struct {
-	Registry Registry
-	Notifier Notifier
-}
-
 // GrizzlyOpts contains options for all Grizzly commands
 type GrizzlyOpts struct {
 	JsonnetPaths []string
