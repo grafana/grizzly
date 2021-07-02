635102d80c0d3f7a5e4809bb17340933b361130f
dsotirakis <sotirakis.dim@gmail.com>
2021-07-02 12:39:24 +0300

    Added integration tests - removed mocks

--- pkg/grafana/dashboards_test.go
+++ pkg/grafana/dashboards_test.go
@@ -1,64 +1,110 @@
 package grafana
 
 import (
-	"errors"
-	"github.com/grafana/grizzly/pkg/grizzly"
-	"github.com/jarcoal/httpmock"
-	"github.com/stretchr/testify/require"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
+	"os/exec"
 	"testing"
+	"time"
+
+	"github.com/grafana/grizzly/pkg/grizzly"
+	"github.com/stretchr/testify/require"
 )
 
-func TestDashboards(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
+func TestDashboard(t *testing.T) {
+	os.Setenv("GRAFANA_URL", "http://localhost:3000")
 
-	_ = os.Setenv("GRAFANA_URL", "url")
-	_ = os.Setenv("GRAFANA_TOKEN", "token")
-	_ = os.Setenv("GRAFANA_USER", "user")
+	go func() {
+		cmd := exec.Command("/bin/sh", "run_container.sh")
+		if o, err := cmd.CombinedOutput(); err != nil {
+			fmt.Printf("fail to spin up container, err: %q", o)
+			os.Exit(1)
+		}
+	}()
 
-	t.Run("valid dashboard response", func(t *testing.T) {
-		expected, err := os.ReadFile("testdata/dashboards/valid_dashboard.json")
-		require.NoError(t, err)
+	defer func() {
+		cmd := exec.Command("/bin/sh", "kill_container.sh")
+		if o, err := cmd.CombinedOutput(); err != nil {
+			fmt.Printf("fail to kill container, err: %q", o)
+			os.Exit(1)
+		}
+	}()
 
-		httpmock.RegisterResponder("GET", "//user:token@url/api/dashboards/uid/uid",
-			httpmock.NewStringResponder(200, string(expected)))
-		resource, err := getRemoteDashboard("uid")
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	timeoutExceeded := time.After(120 * time.Second)
+
+	success := false
+	for !success {
+		select {
+		case <-timeoutExceeded:
+			fmt.Println("failed")
+
+		case <-ticker.C:
+			resp, _ := http.Get("http://localhost:3000/")
+			if resp != nil {
+				success = true
+				break
+			}
+		}
+	}
+
+	t.Run("get remote dashboard - success", func(t *testing.T) {
+		resource, err := getRemoteDashboard("ReciqtgGk")
 		require.NoError(t, err)
 
 		require.Equal(t, resource.APIVersion(), "grizzly.grafana.com/v1alpha1")
-		require.Equal(t, resource.Name(), "uid")
-		require.Equal(t, resource.GetMetadata("folder"), "sample")
+		require.Equal(t, resource.Name(), "ReciqtgGk")
+		require.NotEmpty(t, resource.GetMetadata("folder"))
 	})
 
-	t.Run("invalid response from http call", func(t *testing.T) {
-		httpmock.RegisterResponder("GET", "//user:token@url/api/dashboards/uid/uid",
-			httpmock.NewStringResponder(200, "abc123"))
-		_, err := getRemoteDashboard("uid")
-		require.Error(t, err)
-
-		var apiErr grizzly.APIErr
-		require.True(t, errors.As(err, &apiErr))
+	t.Run("get remote dashboard - not found", func(t *testing.T) {
+		_, err := getRemoteDashboard("dummy")
+		require.EqualError(t, err, "not found")
 	})
 
-	t.Run("valid dashboard response - folderUid doesn't exist", func(t *testing.T) {
-		expectedDashboard, err := os.ReadFile("testdata/dashboards/valid_dashboard.json")
-		expectedFolder := `{ "uid": 4 }`
+	t.Run("get remote dashboard list - success", func(t *testing.T) {
+		list, err := getRemoteDashboardList()
+		fmt.Println(list)
 		require.NoError(t, err)
 
-		httpmock.RegisterResponder("GET", "//user:token@url/api/dashboards/uid/uid",
-			httpmock.NewStringResponder(200, string(expectedDashboard)))
-		httpmock.RegisterResponder("GET", "//user:token@url/api/folder/id/4",
-			httpmock.NewStringResponder(200, expectedFolder))
-		resource, err := getRemoteDashboard("uid")
+		require.Len(t, list, 3)
+		require.EqualValues(t, []string{"ReciqtgGk", "392Ik4GGk", "kE0IIVGGz"}, list)
+	})
+
+	t.Run("post remote dashboard - success", func(t *testing.T) {
+		dashboard, err := os.ReadFile("testdata/test_dashboards/post_dashboard.json")
 		require.NoError(t, err)
 
-		require.Equal(t, resource.APIVersion(), "grizzly.grafana.com/v1alpha1")
-		require.Equal(t, resource.Name(), "uid")
-		require.Equal(t, resource.GetMetadata("folder"), "sample")
+		var resource grizzly.Resource
+
+		err = json.Unmarshal(dashboard, &resource)
+		require.NoError(t, err)
+
+		err = postDashboard(resource)
+		require.NoError(t, err)
+
+		dash, err := getRemoteDashboard("d4sHb0ard-")
+		require.NoError(t, err)
+		require.NotNil(t, dash)
+
+		require.Equal(t, resource.GetMetadata("folder"), "abcdefghi")
+	})
+
+	t.Run("post remote dashboard - success", func(t *testing.T) {
+		var resource grizzly.Resource
+		resource = map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"folder": "dummy",
+			},
+		}
+
+		err := postDashboard(resource)
+		require.EqualError(t, err, "couldn't fetch folder 'dummy' from remote: not found")
 	})
 
 	_ = os.Unsetenv("GRAFANA_URL")
-	_ = os.Unsetenv("GRAFANA_TOKEN")
-	_ = os.Unsetenv("GRAFANA_USER")
 }
