37a43b03efcb81d7c19301a172a60c45068e757f
malcolmholmes <42545407+malcolmholmes@users.noreply.github.com>
2021-10-08 15:03:42 +0100

    Set general folder when pulling dashboards (#177)

--- pkg/grafana/utils.go
+++ pkg/grafana/utils.go
@@ -1,18 +1,21 @@
 package grafana
 
-import "regexp"
+import (
+	"regexp"
+)
 
 var folderURLRegex = regexp.MustCompile("/dashboards/f/([^/]+)")
 
 const generalFolderId = 0
+const generalFolderUID = "general"
 
 func extractFolderUID(d DashboardWrapper) string {
 	folderUid := d.Meta.FolderUID
 	if folderUid == "" {
 		urlPaths := folderURLRegex.FindStringSubmatch(d.Meta.FolderURL)
-		if urlPaths == nil || len(urlPaths) == 0 {
+		if len(urlPaths) == 0 {
 			if d.FolderID == generalFolderId {
-				return ""
+				return generalFolderUID
 			}
 			folder, err := getFolderById(d.FolderID)
 			if err != nil {
