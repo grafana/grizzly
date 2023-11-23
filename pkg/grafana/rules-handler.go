cb18b8e8598982afc5619ffda645892990ed1c04
Malcolm Holmes <mdh@odoko.co.uk>
2023-11-23 22:22:21 +0000

    Simplify prometheus rules

--- pkg/grafana/rules-handler.go
+++ pkg/grafana/rules-handler.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
 	"github.com/grafana/grizzly/pkg/grizzly"
 	"github.com/grafana/tanka/pkg/kubernetes/manifest"
+	"gopkg.in/yaml.v3"
 )
 
 // RuleHandler is a Grizzly Handler for Prometheus Rules
@@ -88,26 +94,140 @@ func (h *RuleHandler) GetUID(resource grizzly.Resource) (string, error) {
 
 // GetByUID retrieves JSON for a resource from an endpoint, by UID
 func (h *RuleHandler) GetByUID(UID string) (*grizzly.Resource, error) {
-	return getRemoteRuleGroup(UID)
+	return h.getRemoteRuleGroup(UID)
 }
 
 // GetRemote retrieves a datasource as a Resource
 func (h *RuleHandler) GetRemote(resource grizzly.Resource) (*grizzly.Resource, error) {
 	uid := fmt.Sprintf("%s.%s", resource.GetMetadata("namespace"), resource.Name())
-	return getRemoteRuleGroup(uid)
+	return h.getRemoteRuleGroup(uid)
 }
 
 // ListRemote retrieves as list of UIDs of all remote resources
 func (h *RuleHandler) ListRemote() ([]string, error) {
-	return getRemoteRuleGroupList()
+	return h.getRemoteRuleGroupList()
 }
 
 // Add pushes a datasource to Grafana via the API
 func (h *RuleHandler) Add(resource grizzly.Resource) error {
-	return writeRuleGroup(resource)
+	return h.writeRuleGroup(resource)
 }
 
 // Update pushes a datasource to Grafana via the API
 func (h *RuleHandler) Update(existing, resource grizzly.Resource) error {
-	return writeRuleGroup(resource)
+	return h.writeRuleGroup(resource)
+}
+
+var cortexTool = func(args ...string) ([]byte, error) {
+	path := os.Getenv("CORTEXTOOL_PATH")
+	if path == "" {
+		var err error
+		path, err = exec.LookPath("cortextool")
+		if err != nil {
+			return nil, err
+		} else if path == "" {
+			return nil, fmt.Errorf("cortextool not found")
+		}
+	}
+	return exec.Command(path, args...).Output()
+}
+
+// getRemoteRuleGroup retrieves a datasource object from Grafana
+func (h *RuleHandler) getRemoteRuleGroup(uid string) (*grizzly.Resource, error) {
+	parts := strings.SplitN(uid, ".", 2)
+	namespace := parts[0]
+	name := parts[1]
+	out, err := cortexTool("rules", "print", "--disable-color")
+	if err != nil {
+		return nil, err
+	}
+	groupings := map[string][]PrometheusRuleGroup{}
+	err = yaml.Unmarshal(out, &groupings)
+	if err != nil {
+		return nil, err
+	}
+	for key, grouping := range groupings {
+		if key == namespace {
+			for _, group := range grouping {
+				if group.Name == name {
+					spec := map[string]interface{}{
+						"rules": group.Rules,
+					}
+					handler := RuleHandler{}
+					resource := grizzly.NewResource(handler.APIVersion(), handler.Kind(), group.Name, spec)
+					resource.SetMetadata("namespace", namespace)
+					return &resource, nil
+				}
+			}
+		}
+	}
+	return nil, grizzly.ErrNotFound
+}
+
+// getRemoteRuleGroupList retrieves a datasource object from Grafana
+func (h *RuleHandler) getRemoteRuleGroupList() ([]string, error) {
+	out, err := cortexTool("rules", "print", "--disable-color")
+	if err != nil {
+		return nil, err
+	}
+	groupings := map[string][]PrometheusRuleGroup{}
+	err = yaml.Unmarshal(out, &groupings)
+	if err != nil {
+		return nil, err
+	}
+
+	IDs := []string{}
+	for namespace, grouping := range groupings {
+		for _, group := range grouping {
+			uid := fmt.Sprintf("%s.%s", namespace, group.Name)
+			IDs = append(IDs, uid)
+		}
+	}
+	return IDs, nil
+}
+
+// PrometheusRuleGroup encapsulates a list of rules
+type PrometheusRuleGroup struct {
+	Namespace string                   `yaml:"-"`
+	Name      string                   `yaml:"name"`
+	Rules     []map[string]interface{} `yaml:"rules"`
+}
+
+// PrometheusRuleGrouping encapsulates a set of named rule groups
+type PrometheusRuleGrouping struct {
+	Namespace string                `json:"namespace"`
+	Groups    []PrometheusRuleGroup `json:"groups"`
+}
+
+func (h *RuleHandler) writeRuleGroup(resource grizzly.Resource) error {
+	tmpfile, err := os.CreateTemp("", "cortextool-*")
+	if err != nil {
+		return err
+	}
+	newGroup := PrometheusRuleGroup{
+		Name: resource.Name(),
+		//Rules: resource.Spec()["rules"].([]map[string]interface{}),
+		Rules: []map[string]interface{}{},
+	}
+	rules := resource.Spec()["rules"].([]interface{})
+	for _, ruleIf := range rules {
+		rule := ruleIf.(map[string]interface{})
+		newGroup.Rules = append(newGroup.Rules, rule)
+	}
+	grouping := PrometheusRuleGrouping{
+		Namespace: resource.GetMetadata("namespace"),
+		Groups:    []PrometheusRuleGroup{newGroup},
+	}
+	out, err := yaml.Marshal(grouping)
+	if err != nil {
+		return err
+	}
+	os.WriteFile(tmpfile.Name(), out, 0644)
+	output, err := cortexTool("rules", "load", tmpfile.Name())
+	if err != nil {
+		log.Println("OUTPUT", output)
+		return err
+	}
+	os.Remove(tmpfile.Name())
+	return err
 }
--- pkg/grafana/rules_test.go
+++ pkg/grafana/rules_test.go
@@ -24,10 +24,12 @@ func TestRules(t *testing.T) {
 			NewProvider(grafanaClient),
 		})
 
+	h := RuleHandler{}
+
 	t.Run("get remote rule group", func(t *testing.T) {
 		mockCortexTool(t, "testdata/list_rules.yaml", nil)
 
-		res, err := getRemoteRuleGroup("first_rules.grizzly_alerts")
+		res, err := h.getRemoteRuleGroup("first_rules.grizzly_alerts")
 		require.NoError(t, err)
 		handler := RuleHandler{}
 		uid, err := handler.GetUID(*res)
@@ -44,7 +46,7 @@ func TestRules(t *testing.T) {
 	t.Run("get remote rule group - error from cortextool client", func(t *testing.T) {
 		mockCortexTool(t, "", errCortextoolClient)
 
-		res, err := getRemoteRuleGroup("first_rules.grizzly_alerts")
+		res, err := h.getRemoteRuleGroup("first_rules.grizzly_alerts")
 		require.Error(t, err)
 		require.Nil(t, res)
 	})
@@ -52,7 +54,7 @@ func TestRules(t *testing.T) {
 	t.Run("get remote rule group - return not found", func(t *testing.T) {
 		mockCortexTool(t, "testdata/list_rules.yaml", nil)
 
-		res, err := getRemoteRuleGroup("name.name")
+		res, err := h.getRemoteRuleGroup("name.name")
 		require.Error(t, err)
 		require.Nil(t, res)
 	})
@@ -60,7 +62,7 @@ func TestRules(t *testing.T) {
 	t.Run("get remote rule group list", func(t *testing.T) {
 		mockCortexTool(t, "testdata/list_rules.yaml", nil)
 
-		res, err := getRemoteRuleGroupList()
+		res, err := h.getRemoteRuleGroupList()
 		require.NoError(t, err)
 		require.Equal(t, "first_rules.grizzly_alerts", res[0])
 	})
@@ -68,7 +70,7 @@ func TestRules(t *testing.T) {
 	t.Run("get remote rule group list", func(t *testing.T) {
 		mockCortexTool(t, "", errCortextoolClient)
 
-		res, err := getRemoteRuleGroupList()
+		res, err := h.getRemoteRuleGroupList()
 		require.Error(t, err)
 		require.Nil(t, res)
 	})
@@ -84,7 +86,7 @@ func TestRules(t *testing.T) {
 
 		resource := grizzly.NewResource("apiV", "kind", "name", spec)
 		resource.SetMetadata("namespace", "value")
-		err = writeRuleGroup(resource)
+		err = h.writeRuleGroup(resource)
 		require.NoError(t, err)
 	})
 
@@ -99,7 +101,7 @@ func TestRules(t *testing.T) {
 
 		resource := grizzly.NewResource("apiV", "kind", "name", spec)
 		resource.SetMetadata("namespace", "value")
-		err = writeRuleGroup(resource)
+		err = h.writeRuleGroup(resource)
 		require.Error(t, err)
 	})
 
