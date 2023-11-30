d4908ba60dca0da35061cf211854bf0fa7e5068c
Malcolm Holmes <mdh@odoko.co.uk>
2023-11-30 11:30:40 +0000

    Make testutils non-internal

--- pkg/grafana/folders_test.go
+++ pkg/grafana/folders_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/go-openapi/runtime"
 	"github.com/grafana/grizzly/pkg/grizzly"
-	. "github.com/grafana/grizzly/pkg/internal/testutil"
+	. "github.com/grafana/grizzly/pkg/testutil"
 	"github.com/stretchr/testify/require"
 )
 
