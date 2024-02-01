0868012b169bf43616603b326720a9e900d55ccb
Julien Duchesne <julien.duchesne@grafana.com>
2024-02-01 11:14:28 -0500

    Add new context and make integration tests stricter I added a new context and I noticed that the tests were still passing Turns out the `.Contains` condition on the output was still matching I changed to a strict `Equal` and remove the stderr output from there
    The subpath image is unused so far, but it will be in a future PR

--- integration/utils_test.go
+++ integration/utils_test.go
@@ -1,7 +1,6 @@
 package integration_test
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -33,14 +32,14 @@ func RunTests(t *testing.T, tests []GrizzlyTest) {
 				cwd, _ := os.Getwd()
 				cmd := exec.Command(filepath.Join(cwd, "../grr"), args...)
 				cmd.Dir = test.TestDir
-				output, err := cmd.CombinedOutput()
+				output, err := cmd.Output()
 				if command.ExpectedError != nil {
 					require.Error(t, err, command.ExpectedError)
 				}
 				if command.ExpectedOutput != "" {
 					data, err := os.ReadFile(filepath.Join(test.TestDir, command.ExpectedOutput))
-					require.NoError(t, err, fmt.Sprintf(""))
-					require.Contains(t, string(output), string(data))
+					require.NoError(t, err)
+					require.Equal(t, string(output), string(data))
 				}
 				exitCode := cmd.ProcessState.ExitCode()
 				require.Equal(t, command.ExpectedCode, exitCode, "Exited with %d (%d expected)", exitCode, command.ExpectedCode)
