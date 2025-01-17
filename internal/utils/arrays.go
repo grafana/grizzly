a6176463e70fe76ed4c5cc9c1430339951bd924c
Kévin Gomez <kevin.gomez@grafana.com>
2025-01-17 13:47:15 +0100

    Add some comments

--- internal/utils/arrays.go
+++ internal/utils/arrays.go
@@ -1,5 +1,7 @@
 package utils
 
+// Map creates a new list populated with the results of applying the `mapper`
+// function to every element from `input`.
 func Map[T any, O any](input []T, mapper func(T) O) []O {
 	if input == nil {
 		return nil
